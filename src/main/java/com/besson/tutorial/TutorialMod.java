2cccb0c95b6fa57c1e518e9204e05f955d809f80
北山_Besson <137259636+BeiShanair@users.noreply.github.com>
2026-01-12 14:16:13 +0800

    CREATIVE MODE TABS

--- src/main/java/com/besson/tutorial/TutorialMod.java
+++ src/main/java/com/besson/tutorial/TutorialMod.java
@@ -1,5 +1,6 @@
 package com.besson.tutorial;
 
+import com.besson.tutorial.item.ModCreativeModeTabs;
 import com.besson.tutorial.item.ModItems;
 import net.fabricmc.api.ModInitializer;
 
@@ -21,6 +22,7 @@ public class TutorialMod implements ModInitializer {
 		// Proceed with mild caution.
 
 		ModItems.register();
+		ModCreativeModeTabs.register();
 		LOGGER.info("Hello Fabric world!");
 	}
 }
