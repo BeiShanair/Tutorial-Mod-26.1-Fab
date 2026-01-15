013ba2e06710dedfef525c1d9a7473c27c1afc11
北山_Besson <137259636+BeiShanair@users.noreply.github.com>
2026-01-15 14:55:12 +0800

    BUILDING BLOCK

--- src/main/java/com/besson/tutorial/TutorialModClient.java
+++ src/main/java/com/besson/tutorial/TutorialModClient.java
@@ -1,10 +1,14 @@
 package com.besson.tutorial;
 
+import com.besson.tutorial.block.ModBlocks;
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.rendering.v1.ChunkSectionLayerMap;
+import net.minecraft.client.renderer.chunk.ChunkSectionLayer;
 
 public class TutorialModClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
-
+        ChunkSectionLayerMap.putBlock(ModBlocks.ICE_ETHER_DOOR, ChunkSectionLayer.CUTOUT);
+        ChunkSectionLayerMap.putBlock(ModBlocks.ICE_ETHER_TRAPDOOR, ChunkSectionLayer.CUTOUT);
     }
 }
